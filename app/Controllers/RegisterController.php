7c5010174004ce91e5d00c7d5776d3cd6d9bfde6
unknown <aldoojustino@gmail.com>
2025-02-08 22:55:31 +0800

    push project TA

--- app/Controllers/RegisterController.php
+++ app/Controllers/RegisterController.php
@@ -4,11 +4,48 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 use CodeIgniter\HTTP\ResponseInterface;
+use App\Models\PenggunaModel;
 
 class RegisterController extends BaseController
 {
     public function register()
     {
         return view('register/register');
+    }
+
+    public function proses()
+    {
+        helper(['form', 'url']);
+
+        $validation = \Config\Services::validation();
+
+        $validation->setRules([
+            'nama_depan' => 'required|min_length[3]|max_length[20]',
+            'nama_belakang' => 'required|min_length[3]|max_length[20]',
+            'email' => 'required|valid_email',
+            'password' => 'required|min_length[8]',
+            'konfirmasi_password' => 'required|matches[password]'
+        ]);
+
+        if (!$this->validate($validation->getRules())) {
+            return redirect()->back()->withInput()->with('validation', $this->validator);
+        }
+
+        $nama_depan = $this->request->getPost('nama_depan');
+        $nama_belakang = $this->request->getPost('nama_belakang');
+        $email = $this->request->getPost('email');
+        $password = $this->request->getPost('password');
+
+        $model = new PenggunaModel();
+        $data = [
+            'nama_depan' => $nama_depan,
+            'nama_belakang' => $nama_belakang,
+            'email' => $email,
+            'password' => password_hash($password, PASSWORD_DEFAULT),
+        ];
+
+        $model->save($data);
+
+        return redirect()->to('/register')->with('success', 'Registrasi berhasil!');
     }
 }
